73cf2ac73d0a7e64f79618b953141e6c90c26c4e
David F <Skilliciousboy@icloud.com>
2021-02-27 14:17:00 +0000

    💄 Sidebar buttons + new colour scheme (js -> jsx)

---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Spacer from "react-spacer";
+
+import Logo from "./logo";
+
+import "../styles/Navbar.scss";
+
+export default function Navbar({setAuth}) {
+
+  const logout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    setAuth(false);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="title-section">
+        <Spacer width="5px"/>
+        <Logo width="50px" height="50px"/>
+        <Spacer width="10px"/>
+        <h1 className="site-title">trackdrop</h1>
+      </div>
+      <div className="end-section">
+        <button className="logout-button" onClick={(e) => logout(e)}>Sign Out</button>
+      </div>
+    </nav>
+  )
+}
---
+++ client/src/pages/Feed.jsx
@@ -0,0 +1,142 @@
+import React, { useState, useEffect } from "react";
+
+import { FaHome } from "react-icons/fa";
+import { BiBookmark } from "react-icons/bi";
+import { BsHeartFill } from "react-icons/bs";
+import { AiOutlineUser } from "react-icons/ai";
+
+import Spacer from "react-spacer";
+
+import Navbar from "../components/navbar";
+import Post from "../components/post";
+
+import "../styles/Feed.scss";
+
+export default function Feed({ setAuth }) {
+  const [name, setName] = useState("");
+  const [Posts, setPosts] = useState([]);
+
+  async function getPosts() {
+    try {
+      const response = await fetch("http://localhost:5000/posts/all", {
+        method: "GET"
+      });
+
+      const parseRes = await response.json();
+      parseRes.reverse();
+
+      setPosts(parseRes);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  async function getName() {
+    try {
+      const response = await fetch("http://localhost:5000/dashboard/", {
+        method: "GET",
+        headers: { token: localStorage.token },
+      });
+
+      const parseRes = await response.json();
+
+      setName(parseRes.username);
+      localStorage.setItem('user', parseRes.username);
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  useEffect(() => {
+    getName();
+    getPosts();
+  }, []);
+
+  useEffect(() => {
+    setPosts(Posts);
+  }, [Posts]);
+
+  const postModal = () => {
+    console.log("postModal");
+  }
+  
+  return (
+    <>
+      <Navbar setAuth={setAuth}/>
+      <div className="main">
+        <Spacer height="10px" />
+        <div className="page">
+          <div className="left">
+            <Spacer height="100px" />
+            <div className="buttons">
+              <div className="button">
+                <FaHome className="list-icon" size="32"/>
+                <Spacer width="19px" />
+                <h2>Home</h2>
+                <Spacer width="3px" />
+              </div>
+              <Spacer height="15px" />
+              <div className="button">
+                <BiBookmark className="list-icon" size="32"/>
+                <Spacer width="19px" />
+                <h2>Saved</h2>
+              </div>
+              <Spacer height="20px" />
+              <div className="button">
+                <Spacer width="4px" />
+                <BsHeartFill className="list-icon heart" size="30"/>
+                <Spacer width="22px" />
+                <h2>Liked</h2>
+                <Spacer width="15px" />
+              </div>
+              <Spacer height="20px" />
+              <div className="button">
+                <AiOutlineUser className="list-icon" size="32"/>
+                <Spacer width="19px" />
+                <h2>Profile</h2>
+              </div>
+              <Spacer height="25px" />
+              <div className="button-div">
+                <button className="logout-button" onClick={() => postModal()}>Post</button>
+              </div>
+            </div> 
+          </div>
+          <div className="middle">
+            <div className="header-text-container">
+              <h1 className="header-text">Latest Posts:</h1>
+            </div>
+            <div>
+              {Posts.map(post => (
+                <Post 
+                  key={post.postid} 
+                  title={post.title} 
+                  artist={post.artist}
+                  link={post.link}
+                  author={post.author}
+                  tags={post.tags} />
+              ))}
+            </div>
+          </div>
+          <div className="right">
+
+          </div>
+        </div>
+        {/* <div>
+          <div className="user-section">
+            <div className="icon search">
+              <FaSearch size="29" onClick={(e) => search(e)}/>
+            </div>
+            <Spacer width="20px"/>
+            <div className="icon profile">
+              <FaRegUserCircle size="34" onClick={(e) => profile(e)}/>
+            </div>
+            <Spacer width="20px"/>
+            <div className="icon sign-out">
+              <FaSignOutAlt size="32" onClick={e => logout(e)}/>
+            </div>
+          </div>
+        </div> */}
+      </div>
+    </>
+  );
+};
